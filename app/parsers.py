5ea238d595cfaad43e86e28f743d2dc2079b0529
Jake Martin <jake_martin_@outlook.com>
2024-07-17 22:31:00 +0100

    Catch download speed parse error

--- app/parsers.py
+++ app/parsers.py
@@ -3,7 +3,7 @@ import math
 import requests
 from lxml import etree
 from utils import Preferences
-from humanfriendly import parse_size
+from humanfriendly import parse_size, InvalidSize
 
 
 def default(self, msg_data, create_config):
@@ -251,7 +251,10 @@ def update3(self, msg_data, create_config):
             continue
 
         network_download_text = ' '.join(network[1:3])
-        network_download += round(parse_size(network_download_text) / 1000 / 1000, 1)
+        try:
+            network_download += round(parse_size(network_download_text) / 1000 / 1000, 1)
+        except InvalidSize:
+            pass
         payload_download = {
             'name': 'Download Throughput',
             'unit_of_measurement': 'Mbit/s',
@@ -260,7 +263,10 @@ def update3(self, msg_data, create_config):
         }
 
         network_upload_text = ' '.join(network[3:5])
-        network_upload += round(parse_size(network_upload_text) / 1000 / 1000, 1)
+        try:
+            network_upload += round(parse_size(network_upload_text) / 1000 / 1000, 1)
+        except InvalidSize:
+            pass
         payload_upload = {
             'name': 'Upload Throughput',
             'unit_of_measurement': 'Mbit/s',
